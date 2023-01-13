8e10cda81549189a1945b2c0eacd3f733688afb0
Adarsh Anand <adarsh.anand.20031@iitgoa.ac.in>
2023-01-13 23:02:12 +0530

    Added routes boilerplate

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,24 @@
 import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
-
-const app = express();
-const PORT = 3000 || process.env.PORT;
+// import apiRoutes from "./routes/api.js";
 
 // use dotenv
 dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
 // use morgan
 app.use(morgan("dev"));
-// use json
+// use json to parse request body
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+// Routes
+// app.use("/api", apiRoutes);
 
 // Listen to requests
 app.listen(PORT, () => {
