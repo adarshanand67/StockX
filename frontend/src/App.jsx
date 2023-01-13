ca895ce8becaeb8832260159f1d4af67f9f82716
Adarsh Anand <adarsh.anand.20031@iitgoa.ac.in>
2023-01-14 00:32:04 +0530

    Update navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import Data from "./components/Data";
 import Spinners from "./components/Spinners";
-import Nav from "./container/Nav";
+import NavigationBar from "./container/Nav";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       {/* <h1 className="text-3xl bg-pink-400 font-bold underline">Hello world!</h1> */}
-      <Nav />
+      <NavigationBar />
       <Data />
     </div>
   );
