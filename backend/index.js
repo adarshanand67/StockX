768bb2a3c52ca39933f46cc63ad61157e902b422
Adarsh Anand <adarsh.anand.20031@iitgoa.ac.in>
2023-01-13 23:11:31 +0530

    Connect MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,19 @@
 // Express app
 import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 import morgan from "morgan";
 // import apiRoutes from "./routes/api.js";
 
 // use dotenv
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT;
+const PORT = 3000 || process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+
+// Add this in .env
+// PORT=3000
+// MONGO_URI=mongodb+srv://flipr:flipr@cluster0.8d8eto9.mongodb.net/?retryWrites=true&w=majority
 
 // use morgan
 app.use(morgan("dev"));
@@ -21,7 +27,18 @@ app.get("/", (req, res) => {
 // Routes
 // app.use("/api", apiRoutes);
 
-// Listen to requests
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// Connect to MongoDB and start server
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
