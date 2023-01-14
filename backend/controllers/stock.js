23279b323aaa932f82e6311f0d329de859bf0187
Restyled.io <commits@restyled.io>
2023-01-14 19:31:57 +0000

    Restyled by prettier

--- backend/controllers/stock.js
+++ backend/controllers/stock.js
@@ -1,32 +1,32 @@
 const _ = require("lodash");
 const Stock = require("../model/stock");
-const csv = require('fast-csv');
-const fs = require('fs');
+const csv = require("fast-csv");
+const fs = require("fs");
 
 // filter stocks by ticker and return the list of stocks
 exports.getStock = (req, res) => {
-  console.log("Request parameters " + req.params.stockTicker)
-  Stock.find({name : req.params.stockTicker}, (err, stocks) => {
+  console.log("Request parameters " + req.params.stockTicker);
+  Stock.find({ name: req.params.stockTicker }, (err, stocks) => {
     if (err) {
       return res.status(400).json({
-        error : err,
+        error: err,
       });
     }
-    res.json(stocks)
+    res.json(stocks);
     // return stocks;
     // res.json(stocks);
-  })
+  });
 };
 
 exports.allStocks = (req, res) => {
   Stock.find((err, stocks) => {
     if (err) {
       return res.status(400).json({
-        error : err,
+        error: err,
       });
     }
     res.json(stocks);
-  })
+  });
 };
 
 exports.updateStocks = (req, res) => {
@@ -36,23 +36,23 @@ exports.updateStocks = (req, res) => {
     }
   });
 
-  for (const file of fs.readdirSync('data/')) {
+  for (const file of fs.readdirSync("data/")) {
     // Create a new stream for each file
     fs.createReadStream(`data/${file}`)
-        .pipe(csv.parse({headers : true}))
-        .on('data', (row) => {
-          // Create a new document with the data from the CSV file
-          const document = new Stock(row);
-          // Add name field to the document
-          document.name = file.split('.')[0];
-          // Save the document to the collection
-          document.save((err) => {
-            if (err) {
-              console.error(err);
-            }
-          });
+      .pipe(csv.parse({ headers: true }))
+      .on("data", (row) => {
+        // Create a new document with the data from the CSV file
+        const document = new Stock(row);
+        // Add name field to the document
+        document.name = file.split(".")[0];
+        // Save the document to the collection
+        document.save((err) => {
+          if (err) {
+            console.error(err);
+          }
         });
+      });
   }
 
   res.json("Stocks updated");
-}
+};
