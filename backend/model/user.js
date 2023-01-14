3c3c1b337b6dc52ae17fca0dbec73f89789270ae
Restyled.io <commits@restyled.io>
2023-01-14 06:56:28 +0000

    Restyled by prettier

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,52 +1,55 @@
 const mongoose = require("mongoose");
-const {v4 : uuidv4} = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
-  name : {
-    type : String,
-    trim : true,
-    required : true,
+  name: {
+    type: String,
+    trim: true,
+    required: true,
   },
-  email : {
-    type : String,
-    trim : true,
-    required : true,
+  email: {
+    type: String,
+    trim: true,
+    required: true,
   },
-  hashed_password : {
-    type : String,
+  hashed_password: {
+    type: String,
     // required: true,
   },
-  salt : String,
-  created : {
-    type : Date,
-    default : Date.now,
+  salt: String,
+  created: {
+    type: Date,
+    default: Date.now,
   },
-  updated : Date,
+  updated: Date,
 });
 
-userSchema.virtual("password")
-    .set(function(password) {
-      // create temporary variable
-      this._password = password;
-      // generate timestamp
-      this.salt = uuidv4();
-      // encrypt password
-      this.hashed_password = this.encryptPassword(password);
-    })
-    .get(function() { return this._password; });
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    // create temporary variable
+    this._password = password;
+    // generate timestamp
+    this.salt = uuidv4();
+    // encrypt password
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
 
 userSchema.methods = {
-  authenticate : function(plainText) {
+  authenticate: function (plainText) {
     return this.encryptPassword(plainText) === this.hashed_password;
   },
-  encryptPassword : function(password) {
-    if (!password)
-      return "";
+  encryptPassword: function (password) {
+    if (!password) return "";
     try {
-      return crypto.createHmac("sha256", this.salt)
-          .update(password)
-          .digest("hex");
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(password)
+        .digest("hex");
     } catch (err) {
       return "";
     }
