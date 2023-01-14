21f79d45aa9f3db2e24c20edb710ec96e64e52d7
Aniket Chaudhri <anichaudhri@gmail.com>
2023-01-15 00:59:36 +0530

    Fix data upload error

--- backend/routes/stock.js
+++ backend/routes/stock.js
@@ -2,13 +2,15 @@ const express = require("express");
 const {
     stockByTicker,
     allStocks,
+    updateStocks,
     getStock,
 } = require("../controllers/stock");
-const { requireSignin } = require("../controllers/user");
+// const { requireSignin } = require("../controllers/user");
 
 const router = express.Router();
 
 router.get("/stocks", allStocks);
+router.put("/stocks", updateStocks);
 router.get("/stocks/:stockTicker", getStock);
 
 // router.param("stockTicker", stockByTicker);
