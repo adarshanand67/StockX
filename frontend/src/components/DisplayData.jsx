04d6d3cfbc1f76289d4448993f3b841ec79a9fa0
Adarsh Anand <adarsh.anand.20031@iitgoa.ac.in>
2023-01-14 11:31:26 +0530

    Display data

---
+++ frontend/src/components/DisplayData.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const DisplayData = ({ data }) => {
+  console.log(data);
+  // console.log(data[0]);
+  return (
+    <>
+      {data.map((item, index) => {
+        if (index == 0) {
+          return (
+            <div key={index}>
+              <h1>{item.Date}</h1>
+              <h1>{item.Open}</h1>
+              <h1>{item.High}</h1>
+              <h1>{item.Low}</h1>
+              <h1>{item.Close}</h1>
+              <h1>{item.Volume}</h1>
+            </div>
+          );
+        }
+      })}
+    </>
+  );
+};
+
+export default DisplayData;
