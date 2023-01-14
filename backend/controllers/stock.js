1cbdb5d7da84cdd1ca311204a7f472575362ae25
Aniket Chaudhri <anichaudhri@gmail.com>
2023-01-15 00:28:14 +0530

    get stockbyTicker

--- backend/controllers/stock.js
+++ backend/controllers/stock.js
@@ -1,6 +1,23 @@
 const _ = require("lodash"); 
 const Stock = require("../model/stock");
 
+
+// filter stocks by ticker and return the list of stocks
+exports.getStock = (req, res) => {
+    console.log("Request parameters " + req.params.stockTicker)
+    Stock.find({name: req.params.stockTicker}, (err, stocks) => {
+        if (err) {
+            return res.status(400).json({
+                error: err,
+            });
+        }
+        res.json(stocks)
+        // return stocks;
+        // res.json(stocks);
+    })
+};
+
+
 exports.allStocks = (req, res) => {
     Stock.find((err, stocks) => {
         if (err) {
--- backend/routes/stock.js
+++ backend/routes/stock.js
@@ -1,21 +1,16 @@
 const express = require("express");
-// const {
-//     userById,
-//     allUsers,
-//     getUser,
-//     updateUser,
-//     deleteUser,
-// } = require("../controllers/user");
-const { allStocks } = require("../controllers/stock");
+const {
+    stockByTicker,
+    allStocks,
+    getStock,
+} = require("../controllers/stock");
 const { requireSignin } = require("../controllers/user");
 
 const router = express.Router();
 
 router.get("/stocks", allStocks);
-// router.get("/user/:userId", requireSignin, getUser);
-// router.put("/user/:userId", requireSignin, updateUser);
-// router.delete("/user/:userId", requireSignin, deleteUser);
+router.get("/stocks/:stockTicker", getStock);
 
-// router.param("userId", userById);
+// router.param("stockTicker", stockByTicker);
 
 module.exports = router;
